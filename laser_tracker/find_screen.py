6d24be554abbc20115f1af8dd96fd6be8892dc63
malcolmwilliams <malcolmnwilliams@gmail.com>
2016-09-15 13:28:56 -0400

    Removed superflous comments and obsolete code sections

--- laser_tracker/find_screen.py
+++ laser_tracker/find_screen.py
@@ -1,9 +1,5 @@
-# USAGE
-# python find_screen.py --query queries/query_marowak.jpg
-
 # import the necessary packages
 from pyimagesearch import imutils
-#from skimage import exposure
 import numpy as np
 import argparse
 import cv2
@@ -30,7 +26,6 @@ def find_screen(vid):
         # find contours in the edged image, keep only the largest
         # ones, and initialize our screen contour
 
-        #im2, contours, hierarchy
         (_,cnts, _) = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
         screenCnt = None
@@ -103,103 +98,31 @@ def find_screen(vid):
         M = cv2.getPerspectiveTransform(rect, dst)
         warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
 
-        # convert the warped image to grayscale and then adjust
-        # the intensity of the pixels to have minimum and maximum
-        # values of 0 and 255, respectively
-
-
-        #warp = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
-        #warp = exposure.rescale_intensity(warp, out_range = (0, 255))----------------------------------------------
-
-        # the pokemon we want to identify will be in the top-right
-        # corner of the warped image -- let's crop this region out
-        #(h, w) = warp.shape
-        #(dX, dY) = (int(w * 0.4), int(h * 0.45))
-        #crop = warp[10:dY, w - dX:w - 10]
-
-        # save the cropped image to file
-        #cv2.imwrite("cropped.png", crop)
-        
         cv2.imshow("image", image)
         cv2.imshow("edge", edged)
         cv2.imshow("warp", imutils.resize(warp, height = 300))
         
+        # Loop the function until the 'a' key is pressed. This gives time for camera setup. 
+        # Return the warp matrix when complete
         if cv2.waitKey(33) == ord('a'):
             cv2.destroyAllWindows()
             return M
 
 
 def show_warp(M):
-#    print "show_warp"
-    
     _ , image = vid.read()
-    #image = cv2.imread(args["query"])
-    #ratio = image.shape[0] / 300.0
-    orig = image.copy()
-    image = imutils.resize(image, height = 300)
 
-    # convert the image to grayscale, blur it, and find edges
-    # in the image
-    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #gray = cv2.bilateralFilter(gray, 11, 17, 17)
-    #edged = cv2.Canny(gray, 30, 200)
-    #thresh = edged.copy()
-
-    '''
-
-    _, image = vid.read()
-    
-    #image = cv2.imread(args["query"])
-    #ratio = image.shape[0] / 300.0
-    orig = image.copy()
-    image = imutils.resize(image, height = 300)
-    
-    
-
-    (tl, tr, br, bl) = rect
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-
-    # ...and now for the height of our new image
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-
-    # take the maximum of the width and height values to reach
-    # our final dimensions
-    maxWidth = max(int(widthA), int(widthB))
-    maxHeight = max(int(heightA), int(heightB))
-
-    # construct our destination points which will be used to
-    # map the screen to a top-down, "birds eye" view
-    dst = np.array([
-            [0, 0],
-            [maxWidth - 1, 0],
-            [maxWidth - 1, maxHeight - 1],
-            [0, maxHeight - 1]], dtype = "float32")
-
-    # calculate the perspective transform matrix and warp
-    # the perspective to grab the screen
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
-    
-    #cv2.imshow("image", image)
-    cv2.imshow("warp", imutils.resize(warp, height = 300))
-    '''
-    warp = cv2.warpPerspective(orig, M, (300, 300))
+    warp = cv2.warpPerspective(image, M, (300, 300))
     cv2.imshow("warp", warp)
-    #cv2.imshow("image", image)
-    #cv2.imshow("edge", edged)
-    #cv2.imshow("warp", imutils.resize(warp, height = 300))
 
-
-    if cv2.waitKey(33) == ord('a'):
-        cv2.destroyAllWindows()
-        #return rect
 
 
 warpMatrix = find_screen(vid)
 while (True):
     show_warp(warpMatrix)
 
+    if cv2.waitKey(33) == ord('a'):
+        cv2.destroyAllWindows()
+        break
 
 vid.release()
