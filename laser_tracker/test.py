43a9a44d46d5830cf4c81b655a9c699b6cf649e3
malcolmwilliams <malcolmnwilliams@gmail.com>
2016-09-15 22:24:13 -0400

    Progress on auto laser finder

--- laser_tracker/test.py
+++ laser_tracker/test.py
@@ -4,16 +4,22 @@ import math
 
 drawing = False # true if mouse is pressed
 ix,iy = -1,-1
-size = 512
-
+#size = 512
+#img = np.zeros((size,size,3), np.uint8)
+img = cv2.imread("2016-09-15-171826.jpg", cv2.IMREAD_COLOR)
+img_height, img_width = img.shape[:2]
+target_x, target_y = 0, 0
+zoom_level = 0
 # mouse callback function
 def draw_circle(event,x,y,flags,parami):
-    global ix,iy,drawing,mode, size
+    global ix,iy,drawing,mode, img, target_x, target_y, zoom_level, img_height, img_width
 
     if event == cv2.EVENT_LBUTTONDOWN:
         #drawing = True
         ix,iy = x,y
-
+        target_x += (x-img_width/2)/(2**zoom_level)
+        target_y += (y-img_height/2)/(2**zoom_level)
+        print "final_x, final_y", target_x, target_y
     elif event == cv2.EVENT_LBUTTONUP:
         #drawing = False
         cur_x, cur_y = x, y
@@ -21,32 +27,40 @@ def draw_circle(event,x,y,flags,parami):
         cv2.circle(img,(ix,iy),int(dist),(0,0,255),1)
     
     elif event == cv2.EVENT_RBUTTONDOWN:
-        print x,y
+        print "x, y", x,y
         #zoom in on the target x and y coords.
         #img = cv2.resize(img,(2*width, 2*height))
         res = img.copy()
-        x_min = x-size/4
-        x_max = x+size/4
-        y_min = y-size/4
-        y_max = y+size/4
+        x_min = x-img_width/4
+        x_max = x+img_width/4
+        y_min = y-img_height/4
+        y_max = y+img_height/4
         if(x_min < 0): 
             x_max -= x_min
             x_min = 0
         if(y_min < 0):
             y_max -= y_min
             y_min = 0
-        if(x_max > size):
-            x_min = x_min - x_max + size
+        if(x_max > img_width):
+            x_min = x_min - x_max + img_width
             x_max = size
-        if(y_max > size):
-            y_min = y_min - y_max + size
+        if(y_max > img_height):
+            y_min = y_min - y_max + img_height
             Y_max = size
+        if(zoom_level == 0):
+            target_x += x
+            target_y += y
+        else:
+            target_x += (x-img_width/2)/ (2**zoom_level)
+            target_y += (y-img_height/2)/(2**zoom_level)
+        zoom_level+=1
+        print "target_x, target_y", target_x, target_y
         res = res[ y_min:y_max, x_min:x_max]
         res = cv2.resize(res,None,fx=2, fy=2, interpolation = cv2.INTER_CUBIC)
         #res = res[ (x-size/2):(y-size/2), (x+size/2):(y+size/2) ]
-        cv2.imshow("res", res)
+        img = res
+        #cv2.imshow("res", res)
 
-img = np.zeros((size,size,3), np.uint8)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_circle)
 
